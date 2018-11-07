27b0e0f838166d428f8f9acfb9cf6c32a64f4e92
Thomas Galliker <thomas@bluewin.ch>
2018-11-07 23:59:29 +0100

    Add unit tests for test steps

---
+++ Tests/TestFactory.Tests/TestSteps/ActionStepTests.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using FluentAssertions;
+using TestFactory.TestSteps;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestFactory.Tests.TestSteps
+{
+    public class ActionStepTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public ActionStepTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void ShouldWaitOnWaitStepRun()
+        {
+            // Arrange
+            var actionCalled = false;
+            Action action = () => { actionCalled = true; };
+            ITestStep actionTestStep = new ActionTestStep(action);
+
+            // Act
+            ITestStepResult testStepResult = actionTestStep.Run();
+
+            // Assert
+            this.testOutputHelper.WriteLine(testStepResult.ToString());
+            testStepResult.Should().NotBeNull();
+            //testStepResult.Duration.Should().BeGreaterOrEqualTo(timeSpan);
+
+            actionCalled.Should().BeTrue();
+        }
+    }
+}
---
+++ Tests/TestFactory.Tests/TestSteps/WaitStepTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+using TestFactory.TestSteps;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestFactory.Tests.TestSteps
+{
+    public class WaitStepTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public WaitStepTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void ShouldWaitOnWaitStepRun()
+        {
+            // Arrange
+            var timeSpan = new TimeSpan(0, 0, 1);
+            ITestStep waitStep = new WaitStep(timeSpan);
+
+            // Act
+            ITestStepResult testStepResult = waitStep.Run();
+
+            // Assert
+            this.testOutputHelper.WriteLine(testStepResult.ToString());
+            testStepResult.Should().NotBeNull();
+            //testStepResult.Duration.Should().BeGreaterOrEqualTo(timeSpan);
+        }
+    }
+}
