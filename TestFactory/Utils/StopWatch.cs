8f7ad74cef7bf34e30047e7926fd2e5068ff0c51
Thomas Galliker <thomas@bluewin.ch>
2018-11-09 15:20:16 +0100

    Add disposable stopwatch

---
+++ TestFactory/Utils/StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestFactory.Utils
+{
+    public class StopWatch : IDisposable
+    {
+        private readonly Stopwatch stopwatch = new Stopwatch();
+        private readonly Action<TimeSpan> callback;
+
+        public StopWatch()
+        {
+            this.stopwatch.Start();
+        }
+
+        public StopWatch(Action<TimeSpan> callback) : this()
+        {
+            this.callback = callback;
+        }
+
+        public static StopWatch Start(Action<TimeSpan> callback)
+        {
+            return new StopWatch(callback);
+        }
+
+        public void Dispose()
+        {
+            this.stopwatch.Stop();
+            this.callback?.Invoke(this.Result);
+        }
+
+        public TimeSpan Result => this.stopwatch.Elapsed;
+    }
+}
