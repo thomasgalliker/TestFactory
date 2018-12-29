be5825b076d08fb59c6bdd29e3df33676ef37a57
Thomas Galliker <thomas@bluewin.ch>
2018-12-29 15:12:49 +0100

    Add CompletedTestStep

---
+++ TestFactory/TestSteps/CompletedTestStep.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestFactory.TestSteps
+{
+    public class CompletedTestStep : TestStepBase
+    {
+        public override Task<ITestStepResult> Run()
+        {
+            return Task.FromResult<ITestStepResult>(new TestStepResult(this, duration: TimeSpan.Zero, isSuccessful: true));
+        }
+    }
+}
---
+++ Tests/TestFactory.Tests/TestSteps/CompletedTestStepTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using TestFactory.TestSteps;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestFactory.Tests.TestSteps
+{
+    public class CompletedTestStepTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public CompletedTestStepTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task ShouldRunCompletedTestStep()
+        {
+            // Arrange
+            ITestStep completedTestStep = new CompletedTestStep();
+
+            // Act
+            var testStepResult = await completedTestStep.Run();
+
+            // Assert
+            this.testOutputHelper.WriteLine(testStepResult.ToString());
+
+            testStepResult.Should().NotBeNull();
+            testStepResult.IsSuccessful.Should().BeTrue();
+            testStepResult.Duration.Should().Be(TimeSpan.Zero);
+        }
+    }
+}
