11a2945b5577e87c66023edd08b1af5717b40980
Thomas Galliker <thomas@bluewin.ch>
2018-12-29 14:21:50 +0100

    Bugfix: Run parallel test steps only once (remove duplicate line)

--- TestFactory/ParallelTestStep.cs
+++ TestFactory/ParallelTestStep.cs
@@ -3,41 +3,39 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using TestFactory.Extensions;
 
 namespace TestFactory
 {
-        [DebuggerDisplay("{DebuggerDisplay,nq}")]
-        public class ParallelTestStep : TestStepBase
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public class ParallelTestStep : TestStepBase
+    {
+        public IEnumerable<ITestStep> TestSteps { get; }
+
+        public ParallelTestStep(IEnumerable<ITestStep> parallelTestSteps, string name = null)
+            : base(name)
         {
-            public IEnumerable<ITestStep> TestSteps { get; }
-
-            public ParallelTestStep(IEnumerable<ITestStep> parallelTestSteps, string name = null)
-                : base(name)
-            {
-                this.TestSteps = parallelTestSteps;
-            }
-
-            public override async Task<ITestStepResult> Run()
-            {
-                var parallelTestStep = this;
-                var stopwatch = Stopwatch.StartNew();
-                AggregateException aggregateException = null;
-                var testStepResults = new ITestStepResult[0];
-
-                var allTasks = Task.WhenAll(parallelTestStep.TestSteps.Select(testStep => testStep.Run()));
-                try
-                {
-                    await allTasks;
-                    testStepResults = await Task.WhenAll(parallelTestStep.TestSteps.Select(testStep => testStep.Run()));
-                }
-                catch
-                {
-                    aggregateException = allTasks.Exception;
-                }
-
-                stopwatch.Stop();
-                return new ParallelTestStepResult(this, stopwatch.Elapsed, aggregateException == null, testStepResults, aggregateException);
-            }
+            this.TestSteps = parallelTestSteps;
         }
+
+        public override async Task<ITestStepResult> Run()
+        {
+            var parallelTestStep = this;
+            var stopwatch = Stopwatch.StartNew();
+            AggregateException aggregateException = null;
+            var testStepResults = new ITestStepResult[0];
+
+            var allTasks = Task.WhenAll(parallelTestStep.TestSteps.Select(testStep => testStep.Run()));
+            try
+            {
+                testStepResults = await allTasks;
+            }
+            catch
+            {
+                aggregateException = allTasks.Exception;
+            }
+
+            stopwatch.Stop();
+            return new ParallelTestStepResult(this, stopwatch.Elapsed, aggregateException == null, testStepResults, aggregateException);
+        }
+    }
 }
